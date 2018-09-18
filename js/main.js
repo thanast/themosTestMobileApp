899e7d09ad6c9aa6b05d27c4f3238c74246daaa2
thanast <thanast@infokey.gr>
2018-09-18 12:51:56 +0300

    Part 3: Using Native Notification

--- js/main.js
+++ js/main.js
@@ -13,8 +13,18 @@ var app = {
         });
     },
 
+		showAlert: function (message, title) {
+		    if (navigator.notification) {
+		        navigator.notification.alert(message, null, title, 'OK');
+		    } else {
+		        alert(title ? (title + ": " + message) : message);
+		    }
+		},
+		
     initialize: function() {
-        this.store = new MemoryStore();
+    		this.store = new MemoryStore(function() {
+        	self.showAlert('Store Initialized', 'Info');
+    		});        
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
         this.findByName();
     }
